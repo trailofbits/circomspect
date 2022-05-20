aed9896570581d1f5aaa2bbbaaa8f78d20d824f2
Fredrik Dahlgren <fredrik.dahlgren@trailofbits.com>
2022-05-20 13:24:22 +0200

    Reorganized CFG and IR module APIs.

--- program_structure/src/control_flow_graph/mod.rs
+++ program_structure/src/control_flow_graph/mod.rs
@@ -1,9 +1,11 @@
 pub mod basic_block;
-pub mod cfg;
 pub mod dominator_tree;
 pub mod errors;
 pub mod param_data;
 
+mod cfg;
 mod cfg_impl;
 mod ssa_impl;
 mod unique_vars;
+
+pub use cfg::*;
--- program_structure/src/intermediate_representation/mod.rs
+++ program_structure/src/intermediate_representation/mod.rs
@@ -1,7 +1,9 @@
 pub mod errors;
-pub mod ir;
 pub mod value_meta;
 pub mod variable_meta;
 
 mod expression_impl;
+mod ir;
 mod statement_impl;
+
+pub use ir::*;
