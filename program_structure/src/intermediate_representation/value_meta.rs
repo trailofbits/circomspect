70da75a561a4340f94eb80c47b4415c40cf46ac5
Fredrik Dahlgren <fredrik.dahlgren@trailofbits.com>
2022-05-27 08:38:55 +0200

    Moved ValueMeta and VariableMeta from CFG to IR module.

--- program_structure/src/intermediate_representation/value_meta.rs
+++ program_structure/src/intermediate_representation/value_meta.rs
@@ -2,9 +2,12 @@ use num_bigint::BigInt;
 
 use crate::environment::VarEnvironment;
 
+pub type ValueEnvironment = VarEnvironment<ValueReduction>;
+
 pub trait ValueMeta {
     /// Propagate variable values defined by the environment to each sub-node.
-    fn propagate_values(&mut self, env: &VarEnvironment<ValueReduction>);
+    /// The method returns true if the node (or a sub-node) was updated.
+    fn propagate_values(&mut self, env: &mut VarEnvironment<ValueReduction>) -> bool;
 
     /// Returns true if the node reduces to a constant value.
     #[must_use]
@@ -23,13 +26,12 @@ pub trait ValueMeta {
     fn get_reduces_to(&self) -> Option<&ValueReduction>;
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum ValueReduction {
     Boolean { value: bool },
     FieldElement { value: BigInt },
 }
 
-// Knowledge buckets
 #[derive(Default, Clone)]
 pub struct ValueKnowledge {
     reduces_to: Option<ValueReduction>,
@@ -40,8 +42,10 @@ impl ValueKnowledge {
         ValueKnowledge::default()
     }
 
-    pub fn set_reduces_to(&mut self, reduces_to: ValueReduction) {
+    pub fn set_reduces_to(&mut self, reduces_to: ValueReduction) -> bool {
+        let result = self.reduces_to.is_none();
         self.reduces_to = Option::Some(reduces_to);
+        result
     }
 
     #[must_use]
