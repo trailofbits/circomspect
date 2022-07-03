0b0fb3eee5f0b7c18b65de09e72ec6b476ae7cc7
Fredrik Dahlgren <fredrik.dahlgren@trailofbits.com>
2022-07-03 16:11:26 +0200

    Added bitwise complement analysis pass

---
+++ program_analysis/src/bitwise_complement.rs
@@ -0,0 +1,142 @@
+use log::debug;
+
+use program_structure::cfg::Cfg;
+use program_structure::error_code::ReportCode;
+use program_structure::error_definition::{Report, ReportCollection};
+use program_structure::file_definition::{FileID, FileLocation};
+use program_structure::ir::*;
+
+pub struct BitwiseComplementWarning {
+    file_id: Option<FileID>,
+    file_location: FileLocation,
+}
+
+impl BitwiseComplementWarning {
+    pub fn into_report(self) -> Report {
+        let mut report = Report::info(
+            "The bitwise complement is reduced modulo `p`, which means that `(~x)ᵢ != ~(xᵢ)` in general.".to_string(),
+            ReportCode::FieldElementArithmetic,
+        );
+        if let Some(file_id) = self.file_id {
+            report.add_primary(
+                self.file_location,
+                file_id,
+                "256-bit complement taken here.".to_string(),
+            );
+        }
+        report
+    }
+}
+
+/// The output of `~x` is reduced modulo `p`, which means that individual bits
+/// will typically not satisfy the expected relation `(~x)ᵢ != ~(xᵢ)`. This may
+/// lead to unexpected results if the developer is not careful.
+pub fn find_bitwise_complement(cfg: &Cfg) -> ReportCollection {
+    debug!("running bitwise complement analysis pass");
+    let mut reports = ReportCollection::new();
+    for basic_block in cfg.iter() {
+        for stmt in basic_block.iter() {
+            visit_statement(stmt, &mut reports);
+        }
+    }
+    debug!("{} new reports generated", reports.len());
+    reports
+}
+
+fn visit_statement(stmt: &Statement, reports: &mut ReportCollection) {
+    use Statement::*;
+    match stmt {
+        IfThenElse { cond, .. } => visit_expression(cond, reports),
+        Substitution { rhe, .. } => visit_expression(rhe, reports),
+        Return { value, .. } => visit_expression(value, reports),
+        LogCall { arg, .. } => visit_expression(arg, reports),
+        Assert { arg, .. } => visit_expression(arg, reports),
+        ConstraintEquality { lhe, rhe, .. } => {
+            visit_expression(lhe, reports);
+            visit_expression(rhe, reports);
+        }
+    }
+}
+
+fn visit_expression(expr: &Expression, reports: &mut ReportCollection) {
+    use Expression::*;
+    use ExpressionPrefixOpcode::*;
+    match expr {
+        PrefixOp { meta, prefix_op, .. } if matches!(prefix_op, Complement) => {
+            reports.push(build_report(meta));
+        }
+        PrefixOp { rhe, .. } => {
+            visit_expression(rhe, reports);
+        }
+        InfixOp { lhe, rhe, .. } => {
+            visit_expression(lhe, reports);
+            visit_expression(rhe, reports);
+        }
+        InlineSwitchOp {
+            cond,
+            if_true,
+            if_false,
+            ..
+        } => {
+            visit_expression(cond, reports);
+            visit_expression(if_true, reports);
+            visit_expression(if_false, reports);
+
+        }
+        Call { args, .. } => {
+            for arg in args {
+                visit_expression(arg, reports);
+            }
+        }
+        ArrayInLine { values, .. } => {
+            for value in values {
+                visit_expression(value, reports);
+            }
+        }
+        Variable { access, .. } | Signal { access, .. } => {
+            use Access::*;
+            for index in access {
+                if let ArrayAccess(index) = index {
+                    visit_expression(index, reports);
+                }
+            }
+        }
+        Number(_,_) | Component { .. } | Phi { .. } => (),
+    }
+}
+
+fn build_report(meta: &Meta) -> Report {
+    BitwiseComplementWarning {
+        file_id: meta.get_file_id(),
+        file_location: meta.file_location(),
+    }
+    .into_report()
+}
+
+#[cfg(test)]
+mod tests {
+    use parser::parse_definition;
+
+    use super::*;
+
+    #[test]
+    fn test_bitwise_complement() {
+        let src = r#"
+            function f() {
+                return (1 > 2)? 3: ~4;
+            }
+        "#;
+        validate_reports(src, 1);
+    }
+
+    fn validate_reports(src: &str, expected_len: usize) {
+        // Build CFG.
+        let (cfg, _) = parse_definition(src).unwrap().try_into().unwrap();
+        let cfg = cfg.into_ssa().unwrap();
+
+        // Generate report collection.
+        let reports = find_bitwise_complement(&cfg);
+
+        assert_eq!(reports.len(), expected_len);
+    }
+}
--- program_analysis/src/lib.rs
+++ program_analysis/src/lib.rs
@@ -1,6 +1,7 @@
 use program_structure::cfg::Cfg;
 use program_structure::error_definition::ReportCollection;
 
+mod bitwise_complement;
 mod constant_conditional;
 mod dead_assignments;
 mod field_arithmetic;
@@ -10,6 +11,7 @@ mod signal_assignments;
 pub fn get_analysis_passes<'a>() -> Vec<Box<dyn Fn(&'a Cfg) -> ReportCollection + 'a>> {
     vec![
         Box::new(dead_assignments::find_dead_assignments),
+        Box::new(bitwise_complement::find_bitwise_complement),
         Box::new(signal_assignments::find_signal_assignments),
         Box::new(field_arithmetic::find_field_element_arithmetic),
         Box::new(field_comparisons::find_field_element_comparisons),
