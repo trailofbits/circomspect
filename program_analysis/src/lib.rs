343f304726711bcf12b457089f96f5e6bb60019a
Fredrik Dahlgren <fredrik.dahlgren@trailofbits.com>
2023-01-09 15:41:13 +0100

    Added analysis runner

--- program_analysis/src/lib.rs
+++ program_analysis/src/lib.rs
@@ -1,3 +1,4 @@
+use analysis_context::{AnalysisContext, AnalysisError};
 use program_structure::cfg::Cfg;
 use program_structure::report::ReportCollection;
 
@@ -5,6 +6,7 @@ extern crate num_bigint_dig as num_bigint;
 
 pub mod constraint_analysis;
 pub mod taint_analysis;
+pub mod analysis_context;
 
 // Analysis passes.
 mod bitwise_complement;
@@ -20,19 +22,25 @@ mod unconstrained_division;
 mod side_effect_analysis;
 mod signal_assignments;
 
-pub fn get_analysis_passes<'a>() -> Vec<Box<dyn Fn(&'a Cfg) -> ReportCollection + 'a>> {
+/// An analysis pass is a function which takes an analysis context and a CFG and
+/// returns a set of reports.
+type AnalysisPass<'a> =
+    dyn Fn(&mut dyn AnalysisContext<Error = AnalysisError>, &'a Cfg) -> ReportCollection + 'a;
+
+pub fn get_analysis_passes<'a>() -> Vec<Box<AnalysisPass<'a>>> {
     vec![
-        Box::new(bitwise_complement::find_bitwise_complement),
-        Box::new(signal_assignments::find_signal_assignments),
-        Box::new(definition_complexity::run_complexity_analysis),
-        Box::new(side_effect_analysis::run_side_effect_analysis),
-        Box::new(field_arithmetic::find_field_element_arithmetic),
-        Box::new(field_comparisons::find_field_element_comparisons),
-        Box::new(unconstrained_division::find_unconstrained_division),
-        Box::new(bn128_specific_circuit::find_bn128_specific_circuits),
-        Box::new(unconstrained_less_than::find_unconstrained_less_than),
-        Box::new(constant_conditional::find_constant_conditional_statement),
-        Box::new(under_constrained_signals::find_under_constrained_signals),
-        Box::new(nonstrict_binary_conversion::find_nonstrict_binary_conversion),
+        // Intra-process analysis passes.
+        Box::new(|_, cfg| bitwise_complement::find_bitwise_complement(cfg)),
+        Box::new(|_, cfg| signal_assignments::find_signal_assignments(cfg)),
+        Box::new(|_, cfg| definition_complexity::run_complexity_analysis(cfg)),
+        Box::new(|_, cfg| side_effect_analysis::run_side_effect_analysis(cfg)),
+        Box::new(|_, cfg| field_arithmetic::find_field_element_arithmetic(cfg)),
+        Box::new(|_, cfg| field_comparisons::find_field_element_comparisons(cfg)),
+        Box::new(|_, cfg| unconstrained_division::find_unconstrained_division(cfg)),
+        Box::new(|_, cfg| bn128_specific_circuit::find_bn128_specific_circuits(cfg)),
+        Box::new(|_, cfg| unconstrained_less_than::find_unconstrained_less_than(cfg)),
+        Box::new(|_, cfg| constant_conditional::find_constant_conditional_statement(cfg)),
+        Box::new(|_, cfg| under_constrained_signals::find_under_constrained_signals(cfg)),
+        Box::new(|_, cfg| nonstrict_binary_conversion::find_nonstrict_binary_conversion(cfg)),
     ]
 }
